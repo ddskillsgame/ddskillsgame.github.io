9a1d8261aaa79798c1c9ae65af9e12a47dd31893
Skills Game (Chip) <dd.skills.game@gmail.com>
2016-10-02 14:07:30 -0600

    first publish!

---
+++ api/config.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Database configuration
+ */
+define('DB_USERNAME', 'webuser'); /* webuser@localhost */
+define('DB_PASSWORD', 'L%7-seRr>O$anSbV');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'SkillsDB');
+
+?>
---
+++ app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: '<h1>D&amp;D Skills Game</h1>'
+})
+export class AppComponent { }
